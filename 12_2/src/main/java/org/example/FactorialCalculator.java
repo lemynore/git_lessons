0f868f9efecd96f6944083e3d4ca39af2fae7a7b
lemynore <liza.dombrovskaya2003@mail.ru>
2024-09-09 16:12:27 +0300

    Initial commit

---
+++ 12_2/src/main/java/org/example/FactorialCalculator.java
@@ -0,0 +1,19 @@
+package org.example;
+public class FactorialCalculator {
+
+    public static long factorial(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Number must be non-negative.");
+        }
+        long result = 1;
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println("Factorial of " + number + " is: " + factorial(number));
+    }
+}
